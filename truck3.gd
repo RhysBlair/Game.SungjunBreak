09878549fe2942a97e58aeb0f0da30062742b7e0
이승철 <cjf6865@naver.com>
2024-12-21 13:46:58 +0900

    test2

---
+++ truck3.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+func destroy():
+	queue_free()
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_entered(area):
+	#오브젝트에 닿으면 발생하는 이벤트
+	print("111")
+	if area.is_in_group("trucks"):
+		destroy()
+		$AnimatedSprite2D.animation = "destroy()"
+		
+		
+func _on_body_entered(body) -> void:
+	print("12345")
+	if body.is_in_group("trucks"):
+		destroy()
+		$AnimatedSprite2D.animation = "destroy()"
+	
